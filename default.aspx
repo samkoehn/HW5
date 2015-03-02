8bb797071511774ee34e1bb328abf8cbf03bc3e1
Samantha Koehn <samantha-koehn@uiowa.edu>
2015-03-01 22:06:16 -0600

    in new visual basic

--- default.aspx
+++ default.aspx
@@ -1,46 +1,16 @@
-﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="default.aspx.vb" Inherits="_Default" %>
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-
-    <title>Loan Calculator With Amortization Table</title>
-    <link rel="stylesheet" type="text/css" href="CSS.css" />
-
-
+    <title></title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
     
-        Sam&#39;s Loan Calculator
-        
-        <br /><br />
-     
-        Loan Amount:<asp:TextBox ID="tbLoanAmt" runat="server" ></asp:TextBox>
-                  
-        <br /><br />      
-        
-        Annual Interest %: <asp:TextBox ID="tbAnnualInterest" runat="server" ></asp:TextBox>
-        
-        <br /><br />
-
-        Loan Term (Yrs): <asp:TextBox ID="tbLoanTerm" runat="server" ></asp:TextBox>
-        
-        <br /><br />
-
-        <asp:Button ID="btnCalcPmt" runat="server" Text="Calculate" />
-        
-        <br /><br />
-                
-        Monthly Payment: &nbsp; <asp:Label ID="lblMonthlyPmt" runat="server"></asp:Label>
-        
-        <br /><br />
-        
-        <asp:GridView ID="loanGridView" runat="server" />
-            
-        </div>
+    </div>
     </form>
 </body>
 </html>
--- default.aspx.vb
+++ default.aspx.vb
@@ -1,83 +1,85 @@
 ﻿'Be sure to import system.data so you can create datatables on the fly.
 Imports System.Data
-
 Partial Class _Default
     Inherits System.Web.UI.Page
-    'Adapted from the loan calculator found at www.dreamincode.net/forums/topic/237228-looping-issues-using-a-grid-for-mortgage-calculator-amortization/
-    Protected Sub btnCalcPmt_Click(sender As Object, e As EventArgs) Handles btnCalcPmt.Click
-        Dim loanAmount As Double
-        Dim annualRate As Double
-        Dim interestRate As Double
-        Dim term As Integer
-        Dim loanTerm As Integer
-        Dim monthlyPayment As Double
 
-        'This section is declaring the variables for loan amortization.
-        Dim interestPaid As Double
-        Dim nBalance As Double
-        Dim principal As Double
+        'Adapted from the loan calculator found at www.dreamincode.net/forums/topic/237228-looping-issues-using-a-grid-for-mortgage-calculator-amortization/
+        Protected Sub btnCalcPmt_Click(sender As Object, e As EventArgs) Handles btnCalcPmt.Click
+            Dim loanAmount As Double
+            Dim annualRate As Double
+            Dim interestRate As Double
+            Dim term As Integer
+            Dim loanTerm As Integer
+            Dim monthlyPayment As Double
 
-        'Declaring a table to hold the payment information.
-        Dim table As DataTable = New DataTable("ParentTable")
-        Dim loanAmortTbl As DataTable = New DataTable("AmortizationTable")
-        Dim tRow As DataRow
+            'This section is declaring the variables for loan amortization.
+            Dim interestPaid As Double
+            Dim nBalance As Double
+            Dim principal As Double
 
-        'This section adds default values to the variables.  
-        interestPaid = 0.0
+            'Declaring a table to hold the payment information.
+            Dim table As DataTable = New DataTable("ParentTable")
+            Dim loanAmortTbl As DataTable = New DataTable("AmortizationTable")
+            Dim tRow As DataRow
 
-        'This section converts each input string to the appropriate variable assigned.
-        loanAmount = CDbl(tbLoanAmt.Text)
-        annualRate = CDbl(tbAnnualInterest.Text)
-        term = CDbl(tbLoanTerm.Text)
+            'This section adds default values to the variables.  
+            interestPaid = 0.0
 
-        'This section formats the loan input to currency.
-        tbLoanAmt.Text = FormatCurrency(loanAmount)
+            'This section converts each input string to the appropriate variable assigned.
+            loanAmount = CDbl(tbLoanAmt.Text)
+            annualRate = CDbl(tbAnnualInterest.Text)
+            term = CDbl(tbLoanTerm.Text)
 
-        'Converting the annual rate to a monthly rate by dividing by 100 * 12 (months in a year)
-        interestRate = annualRate / (100 * 12)
+            'This section formats the loan input to currency.
+            tbLoanAmt.Text = FormatCurrency(loanAmount)
 
-        'Converting the years (term) into months (loan term) by multipling the years by 12.
-        loanTerm = term * 12
+            'Converting the annual rate to a monthly rate by dividing by 100 * 12 (months in a year)
+            interestRate = annualRate / (100 * 12)
 
-        'Calculating the monthly payment using the converted interest rate and loan term.
-        monthlyPayment = loanAmount * interestRate / (1 - Math.Pow((1 + interestRate), (-loanTerm)))
+            'Converting the years (term) into months (loan term) by multipling the years by 12.
+            loanTerm = term * 12
 
-        'Displaying the monthly payment in the textbox and converts the variable to currency.
-        lblMonthlyPmt.Text = FormatCurrency(monthlyPayment)
+            'Calculating the monthly payment using the converted interest rate and loan term.
+            monthlyPayment = loanAmount * interestRate / (1 - Math.Pow((1 + interestRate), (-loanTerm)))
 
-
-        'Adds items to list box, formats them for currency and adds pad spacing for each item.
-        loanAmortTbl.Columns.Add("Payment Number", System.Type.GetType("System.String"))
-        loanAmortTbl.Columns.Add("Principal Paid", System.Type.GetType("System.String"))
-        loanAmortTbl.Columns.Add("Interest Paid", System.Type.GetType("System.String"))
+            'Displaying the monthly payment in the textbox and converts the variable to currency.
+            lblMonthlyPmt.Text = FormatCurrency(monthlyPayment)
 
 
-        'This section uses the for loop to display the loan balance and interest paid over the term of the loan.
-        Dim counterStart As Integer
-
-        For counterStart = 1 To loanTerm
-
-            'Performs calculations for amortization of loan.
-            interestPaid = loanAmount * interestRate
-            principal = monthlyPayment - interestPaid
-            nBalance = loanAmount - principal
-            loanAmount = nBalance
-
-            'Writes the data to a new row in the gridview.
-            tRow = loanAmortTbl.NewRow()
-            tRow("Payment Number") = String.Format(counterStart)
-            tRow("Principal Paid") = String.Format("{0:C}", principal) ' String.Format("{0:C},principal) formats the variable "prinicpal" as currency (C).
-            tRow("Interest Paid") = String.Format("{0:C}", interestPaid)
-            loanAmortTbl.Rows.Add(tRow)
-
-            'Loops to next counterStart (Continues loop until counterStart requirements are met (loanTerm)).
-        Next counterStart
+            'Adds items to list box, formats them for currency and adds pad spacing for each item.
+            loanAmortTbl.Columns.Add("Payment Number", System.Type.GetType("System.String"))
+            loanAmortTbl.Columns.Add("Principal Paid", System.Type.GetType("System.String"))
+            loanAmortTbl.Columns.Add("Interest Paid", System.Type.GetType("System.String"))
 
 
-        loanGridView.DataSource = loanAmortTbl
-        loanGridView.DataBind()
+            'This section uses the for loop to display the loan balance and interest paid over the term of the loan.
+            Dim counterStart As Integer
+
+            For counterStart = 1 To loanTerm
+
+                'Performs calculations for amortization of loan.
+                interestPaid = loanAmount * interestRate
+                principal = monthlyPayment - interestPaid
+                nBalance = loanAmount - principal
+                loanAmount = nBalance
+
+                'Writes the data to a new row in the gridview.
+                tRow = loanAmortTbl.NewRow()
+                tRow("Payment Number") = String.Format(counterStart)
+                tRow("Principal Paid") = String.Format("{0:C}", principal) ' String.Format("{0:C},principal) formats the variable "prinicpal" as currency (C).
+                tRow("Interest Paid") = String.Format("{0:C}", interestPaid)
+                loanAmortTbl.Rows.Add(tRow)
+
+                'Loops to next counterStart (Continues loop until counterStart requirements are met (loanTerm)).
+            Next counterStart
 
 
-    End Sub
+            loanGridView.DataSource = loanAmortTbl
+            loanGridView.DataBind()
+
+
+        End Sub
+
+    End Class
 
 End Class
